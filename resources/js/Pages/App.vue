41941bf8e023baa3c76e101bf9cec3d4ca79332e
Uki7991 <tilek.kubanov@gmail.com>
2021-10-19 12:34:32 +0300

    vue tailwind fortify

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <p class="text-lg bg-red-400">hello world</p>
+    <p>dawdawd</p>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
+import { createApp } from "vue";
+import App from "./Pages/App";
+
+const app = createApp(App);
+
+app.mount('#app')
+
 require('./bootstrap');
+
